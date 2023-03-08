8b4f2bbf6dc4cb7c879c1200465b931556c02d25
poojithaprakash <poojitha.19cs118@cambridge.edu.in>
2023-03-08 21:58:15 +0530

    input box commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector('#btn-translate');
+var inptxt= document.querySelector('#txt-input');
+
+
+inptxt.innerText="helo"
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("clicked")
+})
