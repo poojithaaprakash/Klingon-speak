c85c0176d5b339bdc8c5a66f0e1e61b50f89e0d1
poojithaprakash <poojitha.19cs118@cambridge.edu.in>
2023-03-09 21:19:42 +0530

    final code

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 var btnTranslate = document.querySelector('#btn-translate');
 var inptxt= document.querySelector('#txt-input');
+var outputDiv = document.querySelector("#output");
 
+var klingonUrl="https://api.funtranslations.com/translate/klingon.json";
 
-inptxt.innerText="helo"
+function getTransalationURL(input){
+    return klingonUrl +"?"+ "text="+input
+}
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked")
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
+
+btnTranslate.addEventListener("click", 
+    function clickEventHandler() {
+        var txtinp=inptxt.value;
+
+        fetch(getTransalationURL(txtinp)).then(response=>response.json())
+        .then (json=> {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; // output
+           })
+           .catch(errorHandler)
 })
+        
